c217a59f23f97947fb656f8daa24d6f7cc7451e2
dr-scooby <hinurali@hotmail.com>
2024-09-23 20:45:30 -0700

    updated code

--- src/main/java/com/jahcode/SpringChallengeApp/Controller.java
+++ src/main/java/com/jahcode/SpringChallengeApp/Controller.java
@@ -1,10 +1,13 @@
 package com.jahcode.SpringChallengeApp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.List;
 
-
-
+// Controller class in Spring
+@RestController
 public class Controller {
 
     private List<Challenge> challenges = new ArrayList<>();
@@ -12,6 +15,7 @@ public class Controller {
 
 
 
+    @GetMapping("/challenges")
     public List<Challenge> getAllChallenges(){
         return challenges;
     }
