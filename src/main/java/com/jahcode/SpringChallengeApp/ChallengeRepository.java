9a2651394c96f913ad35cc6c247d69ade6e00a62
dr-scooby <hinurali@hotmail.com>
2024-09-27 20:43:10 -0700

    added findByMonthIgnoreCase

--- src/main/java/com/jahcode/SpringChallengeApp/ChallengeRepository.java
+++ src/main/java/com/jahcode/SpringChallengeApp/ChallengeRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // using JPA
 // needs the Object Challenge and the Long is the primary key type
 public interface ChallengeRepository extends JpaRepository<Challenge, Long>{
+
+    // our custom defined method for the DB
+    void findByMonthIgnoreCase(String month);
 }
